67d2ef4e6c16dec081ffd40d84ddf6266444f54f
lfernando_lira <lfernando.lira@sanmina.com>
2022-07-26 17:13:11 -0500

    language change fixed

--- src/js/app.js
+++ src/js/app.js
@@ -105,11 +105,15 @@ function scrollNav() {
 
     enlaces.forEach(enlace => {
         enlace.addEventListener('click', function (e) {
+            if(enlace.classList.contains('idioma')){
+            }
+            else{
             e.preventDefault();
 
-            const seccionScroll = e.target.attributes.href.value;
-            const seccion = document.querySelector(seccionScroll);
-            seccion.scrollIntoView({ behavior: "smooth" });
+                const seccionScroll = e.target.attributes.href.value;
+                const seccion = document.querySelector(seccionScroll);
+                seccion.scrollIntoView({ behavior: "smooth" });
+            }
         });
     });
 }
